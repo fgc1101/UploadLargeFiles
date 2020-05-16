6a0f0cfc2629316c100e614b60f77c99d27e41ee
fanguochao <544942125@qq.com>
2020-05-16 15:20:08 +0800

    '提交测试文件'

---
+++ demo.php
@@ -0,0 +1,5 @@
+<?php
+require_once './vendor/autoload.php';
+use UploadLargeFiles\UploadLargeFiles\Tools;
+
+Tools::hello();
---
+++ src/UploadLargeFiles/Tools.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace UploadLargeFiles\UploadLargeFiles;
+
+
+class Tools
+{
+    public function __construct()
+    {
+
+    }
+
+    public static function hello(){
+        echo "<h1>Hello world</h1>";
+    }
+
+}
